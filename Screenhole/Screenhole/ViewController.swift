0fe8d487e48095b189e4c69000450a3419b03519
Pim Coumans <pim@pixelrock.nl>
2017-10-20 20:02:25 +0200

    Image assets and old school layout code

--- Screenhole/Screenhole/ViewController.swift
+++ Screenhole/Screenhole/ViewController.swift
@@ -11,21 +11,42 @@ import Alamofire
 
 class ViewController: UIViewController {
 
-	@IBOutlet weak var imageView: UIImageView?
-	@IBOutlet weak var uploadButton: UIButton?
+	let imageView = UIImageView(image: nil)
+	let uploadButton = UIButton(type: .custom)
+	
+	private let verticalSpacing: CGFloat = 32
+	private let horizontalSpacing: CGFloat = 10
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		view.backgroundColor = .black
+		
+		uploadButton.setImage(#imageLiteral(resourceName: "button"), for: .normal)
+		imageView.layer.cornerRadius = 5
+		imageView.layer.masksToBounds = true
+		
+		self.view.addSubview(imageView)
+		self.view.addSubview(uploadButton)
+	}
 	
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
 		
 		Screenshots.shared.requestLatest { [weak self] (image) in
 			guard let image = image else { return }
-			self?.imageView?.image = image
+			self?.imageView.image = image
+			self?.view.setNeedsLayout()
 		}
 		
 		if !Screenhole.shared.isUserSignedIn {
-			self.uploadButton?.isEnabled = false
+			self.uploadButton.isEnabled = false
 			self.showLoginDialog()
 		}
+	}
+	
+	override var preferredStatusBarStyle: UIStatusBarStyle {
+		return .lightContent
 	}
 	
 	func showLoginDialog() {
@@ -49,7 +70,7 @@ class ViewController: UIViewController {
 				if !succeeded {
 					weakSelf?.showLoginDialog()
 				} else {
-					weakSelf?.uploadButton?.isEnabled = true
+					weakSelf?.uploadButton.isEnabled = true
 				}
 			}
 		})
@@ -69,6 +90,43 @@ class ViewController: UIViewController {
 			} else {
 				print("Upload failed!")
 			}
+		}
+	}
+}
+
+extension ViewController {
+	override func viewWillLayoutSubviews() {
+		super.viewWillLayoutSubviews()
+		
+		let safeAreaInsets: UIEdgeInsets
+		let customSafeAreaInsets = UIEdgeInsets(top: verticalSpacing, left: horizontalSpacing, bottom: verticalSpacing, right: horizontalSpacing)
+		
+		if #available(iOS 11.0, *) {
+			safeAreaInsets = view.safeAreaInsets
+		} else {
+			safeAreaInsets = UIEdgeInsets(top: topLayoutGuide.length, left: 0, bottom: bottomLayoutGuide.length, right: 0)
+		}
+		
+		let areaInsets = UIEdgeInsetsMake(max(safeAreaInsets.top, customSafeAreaInsets.top), max(safeAreaInsets.left, customSafeAreaInsets.left), max(safeAreaInsets.bottom, customSafeAreaInsets.bottom), max(safeAreaInsets.right, customSafeAreaInsets.right))
+		let safeArea = UIEdgeInsetsInsetRect(view.bounds, areaInsets)
+		
+		if let buttonImage = uploadButton.image(for: .normal) {
+			let buttonSize = CGSize(width: min(safeArea.width, buttonImage.size.width), height: buttonImage.size.height)
+			uploadButton.frame = CGRect(origin: CGPoint(x: safeArea.midX - (buttonSize.width / 2), y: safeArea.maxY - buttonSize.height), size: buttonSize)
+		}
+		let imageViewContainerFrame = CGRect(x: uploadButton.frame.minX, y: safeArea.minY, width: uploadButton.frame.width, height: uploadButton.frame.minY - (safeArea.minY + verticalSpacing))
+		if let image = imageView.image {
+			let imageRatio = image.size.width / image.size.height
+			let containerRatio = imageViewContainerFrame.width / imageViewContainerFrame.height
+			let imageSize: CGSize
+			if imageRatio < containerRatio {
+				imageSize = CGSize(width: image.size.width * (imageViewContainerFrame.height / image.size.height), height: imageViewContainerFrame.height)
+			} else {
+				imageSize = CGSize(width: imageViewContainerFrame.width, height: image.size.height * (imageViewContainerFrame.width / image.size.width))
+			}
+			imageView.frame = CGRect(origin: CGPoint(x: imageViewContainerFrame.midX - (imageSize.width / 2), y: imageViewContainerFrame.midY - (imageSize.height / 2)), size: imageSize)
+		} else {
+			imageView.frame = imageViewContainerFrame
 		}
 	}
 }
