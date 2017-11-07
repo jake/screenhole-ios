65430464122d6ba68d53f4dc92692ab4784044b4
Jacob Bijani <jake@users.noreply.github.com>
2017-11-07 13:15:15 -0500

    use current (cropped) version

--- Screenhole/Screenhole/Screenshots/Screenshots.swift
+++ Screenhole/Screenhole/Screenshots/Screenshots.swift
@@ -142,7 +142,7 @@ extension PHAsset {
 			})
 		} else if mediaType == .video {
 			let options: PHVideoRequestOptions = PHVideoRequestOptions()
-			options.version = .original
+			options.version = .current
 			PHImageManager.default().requestAVAsset(forVideo: self, options: options, resultHandler: { (asset, audioMix, info) in
 				if let urlAsset = asset as? AVURLAsset {
 					let localVideoUrl = urlAsset.url
