107505b9e8b99330a526712be67f8a7f20d2cc89
Pim Coumans <pim@pixelrock.nl>
2018-01-24 19:29:30 +0100

    Set and get token to and from `WKWebView`

--- Screenhole/Screenhole/UI/RootViewController.swift
+++ Screenhole/Screenhole/UI/RootViewController.swift
@@ -12,32 +12,52 @@ import WebKit
 class RootViewController: UIViewController {
 	
 	@IBOutlet weak var tabBar: UIView?
+	@IBOutlet weak var postButton: UIButton?
 	@IBOutlet weak var webViewContainer: UIView?
-	let webView = WKWebView()
+	var webView: WKWebView?
 	
 	override func viewDidLoad() {
+		postButton?.isEnabled = false
+		
 		guard let container = webViewContainer else {
 			return
 		}
-		container.addSubview(webView)
-		webView.isOpaque = false
-		webView.frame = container.bounds
-		webView.topAnchor.constraint(equalTo: container.topAnchor).isActive = true
-		webView.bottomAnchor.constraint(equalTo: container.bottomAnchor).isActive = true
-		webView.leftAnchor.constraint(equalTo: container.leftAnchor).isActive = true
-		webView.rightAnchor.constraint(equalTo: container.rightAnchor).isActive = true
-		webView.navigationDelegate = self
-		webView.allowsBackForwardNavigationGestures = true
-		webView.scrollView.scrollsToTop = true
-		webView.scrollView.bounces = false // makes sure the top bar doesn't scroll
 		
-		if #available(iOS 11, *) {
-			webView.configuration.setURLSchemeHandler(self, forURLScheme: "screenhole")
+		if let configuration = authenticationConfiguration() {
+			webView = WKWebView(frame: container.bounds, configuration: configuration)
+		} else {
+			webView = WKWebView(frame: container.bounds)
 		}
 		
-		if let url = URL(string: "https://screenhole.net") {
-			let request = URLRequest(url: url)
-			webView.load(request)
+		if let webView = webView {
+			container.addSubview(webView)
+			webView.isOpaque = false
+			webView.frame = container.bounds
+			webView.topAnchor.constraint(equalTo: container.topAnchor).isActive = true
+			webView.bottomAnchor.constraint(equalTo: container.bottomAnchor).isActive = true
+			webView.leftAnchor.constraint(equalTo: container.leftAnchor).isActive = true
+			webView.rightAnchor.constraint(equalTo: container.rightAnchor).isActive = true
+			webView.navigationDelegate = self
+			webView.allowsBackForwardNavigationGestures = true
+			webView.scrollView.scrollsToTop = true
+			webView.scrollView.bounces = false // makes sure the top bar doesn't scroll
+			
+			if let url = URL(string: Screenhole.shared.frontend) {
+				let request = URLRequest(url: url)
+				webView.load(request)
+			}
+		}
+		
+		NotificationCenter.default.addObserver(forName: Notification.Name.UIApplicationDidBecomeActive, object: nil, queue: nil) { notification in
+			Screenhole.shared.refreshUser({ succeeded in
+				self.postButton?.isEnabled = Screenhole.shared.isUserSignedIn
+			})
+		}
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		Screenhole.shared.refreshUser { isLoggedIn in
+			self.postButton?.isEnabled = Screenhole.shared.isUserSignedIn
 		}
 	}
 	
@@ -51,31 +71,38 @@ class RootViewController: UIViewController {
 }
 
 extension RootViewController {
+	func authenticationConfiguration() -> WKWebViewConfiguration? {
+		guard let token = Screenhole.shared.authenticationToken else {
+			return nil
+		}
+		let configuration = WKWebViewConfiguration()
+		let contentController = WKUserContentController();
+		
+		let userScript = WKUserScript(source: "javascript: localStorage.setItem('default_auth_token', '\(token)')", injectionTime: .atDocumentStart, forMainFrameOnly: false)
+		contentController.addUserScript(userScript)
+		configuration.userContentController = contentController
+		return configuration
+	}
+}
+
+extension RootViewController {
 	@IBAction func didPressPostButton(_ button: UIButton) {
 		let postViewController = PostViewController()
 		present(postViewController, animated: true, completion: nil)
 	}
 }
 
-@available(iOS 11.0, *)
-extension RootViewController: WKURLSchemeHandler {
-	func webView(_ webView: WKWebView, start urlSchemeTask: WKURLSchemeTask) {
-		print("SCHEMING: \(urlSchemeTask.request.allHTTPHeaderFields)")
-	}
-	func webView(_ webView: WKWebView, stop urlSchemeTask: WKURLSchemeTask) {
-		print("END SCHEMING!: \(urlSchemeTask)")
-	}
-}
-
 extension RootViewController: WKNavigationDelegate {
 	func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-		if #available(iOS 11, *) {
-			// nothing
-		} else if navigationAction.request.url?.scheme == "screenhole" {
-			decisionHandler(.cancel)
+		if navigationAction.request.url?.scheme == "screenhole",
+			let pathComponents = navigationAction.request.url?.relativePath.components(separatedBy: "/").filter({ $0.count > 0 }),
+			let jwt = pathComponents.last, jwt.count > 10 {
 			// Handle manually
-			print("Handle the scheme: \(navigationAction.request.allHTTPHeaderFields)")
+			Screenhole.shared.authenticationToken = jwt
+			decisionHandler(.cancel)
+			return
 		}
+		
 		decisionHandler(.allow)
 	}
 }
